bd20186745eeaf7a8dc1b5bd68dabd80abb12713
Tom Yam <tomyam@tepez.co.il>
2020-11-01 23:38:21 +0200

    chore: add typings from @types/gulp-jasmine

---
+++ index.d.ts
@@ -0,0 +1,41 @@
+interface JasmineOptions {
+	/**
+	 *  Display spec names in default reporter.
+	 */
+	verbose?: boolean;
+
+	/**
+	 * Include stack traces in failures in default reporter.
+	 * @default false
+	 */
+	includeStackTrace?: boolean;
+
+	/**
+	 * Reporter(s) to use.
+	 */
+	reporter?: jasmine.CustomReporter | ReadonlyArray<jasmine.CustomReporter>;
+
+	/**
+	 * Time to wait in milliseconds before a test automatically fails.
+	 * @default 5000
+	 */
+	timeout?: number;
+
+	/**
+	 * Stops the stream on failed tests.
+	 * @default true
+	 */
+	errorOnFail?: boolean;
+
+	/**
+	 * Passes the config to Jasmine's loadConfig method.
+	 */
+	config?: object;
+}
+
+/**
+ * Executes Jasmine tests. Emits a 'jasmineDone' event on success.
+ * @param options Optional options for the execution of the Jasmine test
+ */
+declare function gulpJasmine(options?: JasmineOptions): NodeJS.ReadWriteStream;
+export = gulpJasmine;
