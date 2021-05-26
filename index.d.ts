e43681ccb145a0152bd6289b3468a51ceabda6a0
Tom Yam <tomyam@tepez.co.il>
2021-05-26 13:46:11 +0300

    feat: use config types from @types/jasmine + export config options

--- index.d.ts
+++ index.d.ts
@@ -1,41 +1,46 @@
-interface JasmineOptions {
-	/**
-	 *  Display spec names in default reporter.
-	 */
-	verbose?: boolean;
+import EnvConfiguration = jasmine.EnvConfiguration;
 
-	/**
-	 * Include stack traces in failures in default reporter.
-	 * @default false
-	 */
-	includeStackTrace?: boolean;
+declare namespace gulpJasmine {
+	interface JasmineOptions {
+		/**
+		 *  Display spec names in default reporter.
+		 */
+		verbose?: boolean;
 
-	/**
-	 * Reporter(s) to use.
-	 */
-	reporter?: jasmine.CustomReporter | ReadonlyArray<jasmine.CustomReporter>;
+		/**
+		 * Include stack traces in failures in default reporter.
+		 * @default false
+		 */
+		includeStackTrace?: boolean;
 
-	/**
-	 * Time to wait in milliseconds before a test automatically fails.
-	 * @default 5000
-	 */
-	timeout?: number;
+		/**
+		 * Reporter(s) to use.
+		 */
+		reporter?: jasmine.CustomReporter | ReadonlyArray<jasmine.CustomReporter>;
 
-	/**
-	 * Stops the stream on failed tests.
-	 * @default true
-	 */
-	errorOnFail?: boolean;
+		/**
+		 * Time to wait in milliseconds before a test automatically fails.
+		 * @default 5000
+		 */
+		timeout?: number;
 
-	/**
-	 * Passes the config to Jasmine's loadConfig method.
-	 */
-	config?: object;
+		/**
+		 * Stops the stream on failed tests.
+		 * @default true
+		 */
+		errorOnFail?: boolean;
+
+		/**
+		 * Passes the config to Jasmine's loadConfig method.
+		 */
+		config?: EnvConfiguration;
+	}
 }
 
 /**
  * Executes Jasmine tests. Emits a 'jasmineDone' event on success.
  * @param options Optional options for the execution of the Jasmine test
  */
-declare function gulpJasmine(options?: JasmineOptions): NodeJS.ReadWriteStream;
+declare function gulpJasmine(options?: gulpJasmine.JasmineOptions): NodeJS.ReadWriteStream;
+
 export = gulpJasmine;
